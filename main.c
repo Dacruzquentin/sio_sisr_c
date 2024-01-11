fb7b6a4362abddc6113c144569152589a617f2a3
Dacruzquentin <147733740+Dacruzquentin@users.noreply.github.com>
2024-01-11 15:57:35 +0000

    v 0.1.0 : add salutation function + greater function + test

--- main.c
+++ main.c
@@ -1,8 +1,45 @@
 
 #include <stdio.h>
 
+void salutation()
+{
+  printf("Hello World\n");
+}
+ 
+int greater(int x, int y)
+{
+  if(x > y)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
 int main()
 {
-       	printf("Hello Word\n");
-	return 0;
+       	salutation();
+ 
+       	int num1 = 4;
+       	int num2 = 5;
+
+       	int age, limiteAge;
+          
+       	age = 16;
+	limiteAge = 18;
+	
+	// %d or %i is used for display integer variable ( int )
+ 
+	printf("%d\n", num1);
+	printf("%d\n", num2);
+
+	int resultat = -1;
+	printf("%d\n", resultat);
+	resultat = greater(num1, num2);
+	printf("resultat = %i / greater = %d \n ", greater(4,5));
+
+ return 0;
+
 }
